2e3dece44f0ea0d693f5d93ab7f148867287490c
Max Bronckers <maxbronckers@me.com>
2023-04-25 09:01:16 +0200

    Remove Python 3.6 support
    Ruff requires Python 3.7 or newer.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from pathlib import Path
 
 from setuptools import setup
 
-if sys.version_info < (3, 6, 0):
-    raise RuntimeError("ib_insync requires Python 3.6 or higher")
+if sys.version_info < (3, 7, 0):
+    raise RuntimeError("ib_insync requires Python 3.7 or higher")
 
 here = Path(__file__).parent.resolve()
 
@@ -26,13 +26,12 @@ setup(
     author='Ewald R. de Wit',
     author_email='ewald.de.wit@gmail.com',
     license='BSD',
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Office/Business :: Financial :: Investment',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -44,7 +43,6 @@ setup(
     packages=['ib_insync'],
     package_data={'ib_insync': ['py.typed']},
     install_requires=['eventkit', 'nest_asyncio',
-                      'dataclasses;python_version<"3.7"',
                       'backports.zoneinfo;python_version<"3.9"'],
     setup_requires=['flake8']
 )
